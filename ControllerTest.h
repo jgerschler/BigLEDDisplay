c2b7306253992b17d2763b939a0271e6b83389c2
jgerschler <jjgerschler@gmail.com>
2017-01-26 11:39:54 -0600

    script for testing control board

--- ControllerTest.h
+++ ControllerTest.h
@@ -4,7 +4,6 @@ Segment letters begin at top segment (A), going clockwise. Center segment is G.
 */
 
 // segment  - pin number 
-
 #define A 22
 #define B 23
 #define C 24
@@ -14,64 +13,51 @@ Segment letters begin at top segment (A), going clockwise. Center segment is G.
 #define G 28
 
 // common anodes
-
 #define ANODE1 50
 #define ANODE2 51
 #define ANODE3 52
 #define ANODE4 53
 
-int timer=0;
-
-int i=0;
-
 // digit display functions
-
 void digit8() {
-	
-	digitalWrite(A, HIGH);
-	digitalWrite(B, HIGH);
-	digitalWrite(C, HIGH);
-	digitalWrite(D, HIGH);
-	digitalWrite(E, HIGH);
-	digitalWrite(F, HIGH);
-	digitalWrite(G, HIGH);
-	
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
 };
-
-// setup
 
 void setup() {
-	
-	pinMode(A, OUTPUT);
-	pinMode(B, OUTPUT);
-	pinMode(C, OUTPUT);
-	pinMode(D, OUTPUT);
-	pinMode(E, OUTPUT);
-	pinMode(F, OUTPUT);
-	pinMode(G, OUTPUT);
-	
-	pinMode(ANODE1, OUTPUT);
-	pinMode(ANODE2, OUTPUT);
-	pinMode(ANODE3, OUTPUT);
-	pinMode(ANODE4, OUTPUT);	
-	
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+    pinMode(C, OUTPUT);
+    pinMode(D, OUTPUT);
+    pinMode(E, OUTPUT);
+    pinMode(F, OUTPUT);
+    pinMode(G, OUTPUT);
+    
+    pinMode(ANODE1, OUTPUT);
+    pinMode(ANODE2, OUTPUT);
+    pinMode(ANODE3, OUTPUT);
+    pinMode(ANODE4, OUTPUT);    
+    
+    digit8();    
 };
 
-// main loop
-
 void loop() {
-	
-	timer++;
-	
-	displaySingleDigit(i);
-	
-	if (timer == 10) {
-		
-		timer = 0;
-		i++;
-		
-		if (i>1000) {i = 0;};
-		
-	}
-	
+    digitalWrite(ANODE1, HIGH);
+    delay(1000)
+    digitalWrite(ANODE1, LOW);
+    digitalWrite(ANODE2, HIGH);
+    delay(1000)
+    digitalWrite(ANODE2, LOW);
+    digitalWrite(ANODE3, HIGH);
+    delay(1000)
+    digitalWrite(ANODE3, LOW);
+    digitalWrite(ANODE4, HIGH);
+    delay(1000)
+    digitalWrite(ANODE4, LOW);
+    digitalWrite(ANODE2, HIGH);
 };
