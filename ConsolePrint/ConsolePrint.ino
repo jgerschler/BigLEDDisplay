ec89d42ceb0da240b5c7b65cd6004c5b38895086
jgerschler <jjgerschler@gmail.com>
2017-01-28 09:27:45 -0600

    testing script

---
+++ ConsolePrint/ConsolePrint.ino
@@ -0,0 +1,14 @@
+
+int x = 0;
+int starting_min = 10;
+int starting_sec = starting_min / 60.0
+
+void setup() {
+  Serial.begin(9600);      // open the serial port at 9600 bps:    
+}
+
+void loop() {
+  Serial.print("Random BS");
+  delay(500);
+}
+
