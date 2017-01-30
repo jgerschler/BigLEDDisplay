20a25241a8afb3a4df0d79158c6d93dd2b3dd4d1
jgerschler <jjgerschler@gmail.com>
2017-01-30 11:22:59 -0600

    in progress

--- ConsolePrint/ConsolePrint.ino
+++ ConsolePrint/ConsolePrint.ino
@@ -1,17 +1,19 @@
+#include stdio.h
 
 int x = 0;
 int starting_time = 654;
 int starting_min = starting_time / 60;
 int starting_sec = starting_time % 60;
+unsigned long time;
 
 void setup() {
   Serial.begin(9600);    
 }
 
 void loop() {
-  Serial.print("The time is:\n");
-  Serial.print(String(starting_min)+"\n");
-  Serial.print(starting_sec);
+  Serial.println("The time is:");
+  Serial.println(String(starting_min+":"+String(starting_sec)));
+
   delay(1000);
   starting_sec-=1;
 }
