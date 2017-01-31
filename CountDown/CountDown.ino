0d736ac075c30061e530319184ea91a642e5388c
jgerschler <jjgerschler@gmail.com>
2017-01-31 09:15:59 -0600

    updated

---
+++ CountDown/CountDown.ino
@@ -0,0 +1,26 @@
+int x = 0;
+int starting_time = 654;
+int starting_min = starting_time / 60;
+int starting_sec = starting_time % 60;
+unsigned long time;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("The time is:");
+  Serial.println(String(starting_min+":"+String(starting_sec)));    
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    starting_time-=1;
+    starting_min = starting_time / 60;
+    starting_sec = starting_time % 60;
+    Serial.println("The time is:");
+    Serial.println(String(starting_min)+":"+String(starting_sec));
+  }
+}
+
