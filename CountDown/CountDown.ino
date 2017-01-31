33b9bef6736dfacdcc345f35d0239c095c856daa
jgerschler <jjgerschler@gmail.com>
2017-01-31 09:25:06 -0600

    updated

--- CountDown/CountDown.ino
+++ CountDown/CountDown.ino
@@ -2,7 +2,6 @@ int x = 0;
 int starting_time = 654;
 int starting_min = starting_time / 60;
 int starting_sec = starting_time % 60;
-unsigned long time;
 unsigned long previousMillis = 0;
 const long interval = 1000;
 
